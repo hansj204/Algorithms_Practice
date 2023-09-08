d7764086de99c7277578ff6d32dc49e77b9d3072
hansj <hansj2041@gmail.com>
2023-09-08 17:00:33 +0900

    문제풀이중

---
+++ 01_Graph/01_Representation Of Graph/01_Find a specific city.js
@@ -0,0 +1,42 @@
+// https://www.acmicpc.net/problem/18352
+const filePath = `linux` === process.platform ? `dev/stdin` : `input.txt`;
+const [info, ...distance] = require(`fs`).readFileSync(filePath).toString().trim().split(`\r\n`);
+
+const [N, M, K, X] = info.split(` `).map(Number)
+
+const nodes = Array.from({length: N + 1}, () => []);
+const visited = Array.from({length: N + 1}, () => -1);
+const answer = [];
+
+for(const dis of distance) {
+    const [A , B] = dis.split(` `).map(Number)
+    nodes[A].push(B)
+}
+
+BFS(X);
+
+for(const [key, value] of Object.entries(visited)) {
+    if(value === K) answer.push(key)
+}
+
+if(0 === answer.length) {
+    console.log(-1)
+} else {
+    console.log(answer.sort().join(`\n`));
+}
+
+function BFS(node) {
+    const queue = [node];
+    visited[node]++;
+
+    while(0 < queue.length) {
+        const parentNode = queue.shift();
+
+        for(const childNode of nodes[parentNode]) {
+            if(-1 !== visited[childNode]) continue;
+
+            visited[childNode] = visited[parentNode] + 1;
+            queue.push(childNode);
+         }
+    }
+}
---
+++ 01_Graph/01_Representation Of Graph/02_Hacking.js
@@ -0,0 +1,38 @@
+// https://www.acmicpc.net/problem/1325
+const filePath = `linux` === process.platform ? `dev/stdin` : `input.txt`;
+const lines = require(`fs`).readFileSync(filePath).toString().trim().split(`\r\n`);
+
+const [N, M] = lines[0].split(` `).map(Number)
+const trustList = lines.slice(1).map(line => line.split(` `).map(Number))
+
+const nodes = Array.from({length: N + 1}, () => []);
+const answer = [];
+let visited = [];
+
+for(const trust of trustList) {
+    const [A , B] = trust;
+    nodes[A].push(B)
+}
+
+for(let idx = 1; idx <= N; idx++) {
+    visited = Array.from({length: N + 1}, () => -1);
+    BFS(idx);
+    
+    console.log(visited)
+}
+
+function BFS(node) {
+    const queue = [node];
+    visited[node]++;
+
+    while(0 < queue.length) {
+        const parentNode = queue.shift();
+
+        for(const childNode of nodes[parentNode]) {
+            if(-1 !== visited[childNode]) continue;
+
+            visited[childNode] = visited[parentNode] + 1;
+            queue.push(childNode);
+         }
+    }
+}
